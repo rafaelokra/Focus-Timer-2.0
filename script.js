e5f35608aa6936daa67a09e2b396a6e30116052d
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-06-14 00:52:14 -0300

    faltando alguns ajustes

--- script.js
+++ script.js
@@ -1,7 +1,75 @@
+// Event Listeners para os botões
+const buttonStart = document.querySelector('.play');
+const buttonStop = document.querySelector('.stop');
+const buttonIncrease = document.querySelector('.increase');
+const buttonDecrease = document.querySelector('.todecrease');
+
+buttonStart.addEventListener('click', startTimer);
+buttonStop.addEventListener('click', stopTimer);
+buttonIncrease.addEventListener('click', increaseTime);
+buttonDecrease.addEventListener('click', decreaseTime);
+
+let minutes = 25;
+let seconds = 0;
+let timerInterval;
+let isRunning = false;
+const minutesDisplay = document.querySelector('.minutes');
+const secondsDisplay = document.querySelector('.seconds');
+
+function updateTimerDisplay() {
+  minutesDisplay.textContent = String(minutes).padStart(2, '0');
+  secondsDisplay.textContent = String(seconds).padStart(2, '0');
+}
+
+function startTimer() {
+  if (isRunning) return;
+  
+  timerInterval = setInterval(() => {
+    if (seconds === 0) {
+      if (minutes === 0) {
+        clearInterval(timerInterval);
+        alert('Tempo esgotado!');
+        return;
+      }
+      minutes--;
+      seconds = 59;
+    } else {
+      seconds--;
+    }
+    updateTimerDisplay();
+  }, 1000);
+  
+  isRunning = true;
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  isRunning = false;
+}
+
+function increaseTime() {
+  minutes += 5;
+  updateTimerDisplay();
+}
+function decreaseTime() {
+  minutes -= 5;
+  if (minutes < 0) {
+    minutes = 0;
+  }
+  updateTimerDisplay();
+}
+
+
+
+
+
+
+
+
 
 var audio;
 
-    function adicionarAudioVento() {
+    function adicionarAudioFlorest() {
       if (audio && !audio.paused) {
         audio.pause();
       } else {
@@ -9,27 +77,27 @@ var audio;
         audio.play();
       }
     }
-    function adicionarAudioChuva(){
-        if (audio && !audio.paused) {
-            audio.pause();
-          } else {
-            audio = new Audio('sounds/Chuva.wav'); // 
-            audio.play();
-          }
+    function adicionarAudioChuva() {
+      if (audio && !audio.paused){
+        audio.pause();
+      } else {
+        audio = new Audio('sounds/Chuva.wav');
+        audio.play();
+      }
     }
-    function adicionarAudioHouse(){
-        if (audio && !audio.paused) {
-            audio.pause();
-          } else {
-            audio = new Audio('sounds/Cafeteria.wav'); // 
-            audio.play();
-          }
+    function adicionarAudioCafeteria() {
+      if (audio && !audio.paused){
+        audio.pause();
+      } else {
+        audio = new Audio('sounds/cafeteria.wav');
+        audio.play();
+      }
     }
-    function adicionarAudioCool(){
-        if (audio && !audio.paused) {
-            audio.pause();
-          } else {
-            audio = new Audio('sounds/Lareira.wav'); // 
-            audio.play();
-          }
+    function adicionarAudioLareira() {
+      if (audio && !Audio.paused){
+        audio.pause();
+    } else {
+      audio = new Audio('sounds/lareira.wav');
+      audio.play();
     }
+  }
